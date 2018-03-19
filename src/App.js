d9f68ac58ade68763f0006f238c7fd2d79d6bac9
VadymShkarupylo <shkarupylo.vadym@gmail.com>
2018-03-19 10:42:20 +0200

    7th commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 import ReactTable from 'react-table';
 import './App.css';
 import 'react-table/react-table.css';
+import TrackDetails from './TrackDetails';
 
 const columns = [
 	{
@@ -28,24 +29,21 @@ accessor: 'primaryGenreName'
 },
 {
 Header: '',
-Cell: row => (
-	<div>
-		<button type = "push"><span className="glyphicon glyphicon-plus"></span></button>
-	</div>
-)
+expander: true
 }
 ]
 
 class App extends Component {
-
-
 
 	handleClick() {
 		axios.get('https://itunes.apple.com/search?term='+this._inputElement.value)
 			.then(response => this.setState({
 				artistName: response.data.results[0].artistName,
 				resultCount: response.data.resultCount,
-				results: response.data.results
+				results: response.data.results,
+				collectionPrice: response.data.results[1].collectionPrice,
+				trackPrice: response.data.results[1].trackPrice,
+				trackCount: response.data.results[1].trackCount,
 			}));
 	}
 
@@ -54,10 +52,36 @@ class App extends Component {
 		this.state = {
 			artistName: '',
 			resultCount: '',
-			results: []
+			collectionPrice: '',
+			trackPrice: '',
+			trackCount: '',
+			results: [],
+			expanded: {}
 		}
 		this.handleClick = this.handleClick.bind(this);
 	}
+
+	renderSubComponent = row => {
+        return <TrackDetails trackData={row.trackData}/>;
+    };
+
+  renderExpandedComponent = row => {
+        return (
+            <div>
+                <button type="push">
+                    <span className="glyphicon glyphicon-plus"></span>
+                </button>
+            </div>
+        );
+  };
+
+  onExpandChange = (newExpanded, index, event) => {
+        this.setState({
+            expanded: {
+                [index]: true
+            },
+        });
+  };
 
 	render() {
 		return (
@@ -77,6 +101,10 @@ class App extends Component {
     			columns={columns}
 					defaultPageSize={10}
 					className="-striped -highlight"
+					SubComponent={this.renderSubComponent}
+          ExpanderComponent={this.renderExpandedComponent}
+          onExpandedChange={this.onExpandChange}
+          expanded={this.state.expanded}
   			/>
 			</div>
 		)
---
+++ src/TrackDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrackDetails = props => {
+ const trackDetails = props.trackDetails;
+  return (
+     <div>
+        details
+     </div>
+   )
+};
+
+export default TrackDetails;
